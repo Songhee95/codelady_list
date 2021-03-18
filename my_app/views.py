bbab396ac70a98b92068298f18de03ac15f9278e
songhee <thdgml5537@gmail.com>
2021-03-18 01:20:06 -0400

    use requests and beautifulsoup library to get the data from url

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,14 @@
 import requests
-from django.shortcuts import render
 from bs4 import BeautifulSoup
+# if user put space in the url,
+# this quote_plus will fill the space with + in the url automatically
+from django.shortcuts import render
+from requests.compat import quote_plus
+from . import models
 
 # Create your views here.
+
+BASE_CRAIGSLIST_URL = "https://atlanta.craigslist.org/search/?query={}"
 
 
 def home(request):
@@ -12,6 +18,26 @@ def home(request):
 def new_search(request):
     # request.POST returns dictionary
     search = request.POST.get('search')
+    # all the searched text will be stored as an object in the search model
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    # using requests library
+    response = requests.get(final_url)
+    data = response.text
+    # Use BeautifulSoup to find value from html via className
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': "result-row"})
+    post_title = post_listings[0].find(class_='result-title').text
+    post_url = post_listings[0].find('a').get('href')
+    post_price = post_listings[0].find(class_='result-price').text
+
+    # store the data and send it to front-end
+
+    print(post_title)
+    print(post_url)
+    print(post_price)
+
+    # print(data)
     stuff_for_frontend = {
         'search': search,
     }
