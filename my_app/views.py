69b1e2a5347ec90f8db4b0dbf2a8d3e7653619c1
songhee <thdgml5537@gmail.com>
2021-03-19 12:32:07 -0400

    change html file just in cae

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-# if user put space in the url,
-# this quote_plus will fill the space with + in the url automatically
 from django.shortcuts import render
 from requests.compat import quote_plus
 from . import models
 
-# Create your views here.
-
-BASE_CRAIGSLIST_URL = "https://atlanta.craigslist.org/search/?query={}"
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
@@ -17,42 +13,40 @@ def home(request):
 
 
 def new_search(request):
-    # request.POST returns dictionary
     search = request.POST.get('search')
-    # all the searched text will be stored as an object in the search model
     models.Search.objects.create(search=search)
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
-    # using requests library
     response = requests.get(final_url)
     data = response.text
-    # Use BeautifulSoup to find value from html via className
     soup = BeautifulSoup(data, features='html.parser')
-    post_listings = soup.find_all('li', {'class': "result-row"})
 
-    # store the data and send it to front-end
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
     final_postings = []
 
     for post in post_listings:
         post_title = post.find(class_='result-title').text
         post_url = post.find('a').get('href')
+
         if post.find(class_='result-price'):
             post_price = post.find(class_='result-price').text
         else:
             post_price = 'N/A'
 
         if post.find(class_='result-image').get('data-ids'):
-            post_image = post.find(
-                class_='result-image').get('data-ids').split(',')
-            final_image = []
-            for url in post_image:
-                final_image.append((BASE_IMAGE_URL.format(url.split(':')[1])))
+            post_image_id = post.find(
+                class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
         else:
-            final_image = ['https://craigslist.org/images/peace.jpg']
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
 
-        final_postings.append((post_title, post_url, post_price, final_image))
+        final_postings.append(
+            (post_title, post_url, post_price, post_image_url))
 
     stuff_for_frontend = {
         'search': search,
         'final_postings': final_postings,
     }
+
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
