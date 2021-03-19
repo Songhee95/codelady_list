566190aee60265d115903f3501e410186a40cfb9
songhee <thdgml5537@gmail.com>
2021-03-19 14:14:12 -0400

    style connect

--- codelady_list/settings.py
+++ codelady_list/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-import dj_database_url
 from pathlib import Path
 import os
 import django_heroku
@@ -122,10 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILE_DIRS = [
-    "static/css",
-    "static/js", ]
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
