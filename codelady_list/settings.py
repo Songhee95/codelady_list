4e3d8c38719a697629fe472488fb7f2cd266ecfc
songhee <thdgml5537@gmail.com>
2021-03-19 01:47:33 -0400

    heroku deploy error

--- codelady_list/settings.py
+++ codelady_list/settings.py
@@ -122,10 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 django_heroku.settings(locals())
